0fffdba69d0ef36e98fe3d6ecaed7f338f2f3294
Kimsolchan <khj25830@naver.com>
2023-06-29 23:16:08 +0900

    programmers Ex_12937 modify

--- programmers/lv01/Ex_12937.java
+++ programmers/lv01/Ex_12937.java
@@ -1,12 +1,9 @@
 package codingtest.programmers.lv01;
 
 public class Ex_12937 {
-	public static String solution(int num) {
-		String answer = (num % 2 == 0) ? "Odd" : "Even";
-		return answer;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(solution(4));
-	}
+    public String solution(int num) {
+    	// answer = 조건(num % 2 == 0)의 결과가 true면 "Even" / false면 "Odd"
+        String answer = (num % 2 == 0) ? "Even" : "Odd";
+        return answer;
+    }
 }
