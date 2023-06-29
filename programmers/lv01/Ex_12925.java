b822ce9db2c1b7d2c02966f31aece199c14ab021
Kimsolchan <khj25830@naver.com>
2023-06-30 02:32:13 +0900

    programmers Ex_12925 commit

---
+++ programmers/lv01/Ex_12925.java
@@ -0,0 +1,10 @@
+package codingtest.programmers.lv01;
+
+public class Ex_12925 {
+	public int solution(String s) {
+		// 문자열 s를 정수형태로 변환하여 answer에 저장한다.
+		int answer = Integer.parseInt(s);
+		
+		return answer;
+	}
+}
--- programmers/lv01/Test.java
+++ programmers/lv01/Test.java
@@ -2,18 +2,7 @@ package codingtest.programmers.lv01;
 
 public class Test {
 	public static void main(String[] args) {
-		long l = 123456;
-		String str = Long.toString(l);
-		String[] strArr = str.split("");
-		
-		int[] intArr = new int[strArr.length];
-		
-		for(int i=0, j=intArr.length-1; j>=0; i++, j--) {
-			intArr[i] = Integer.parseInt(strArr[j]);
-		}
-		
-		for(int i=0; i < intArr.length; i++) {
-			System.out.print(intArr[i]);
-		}
+		int a = -123456;
+		System.out.println(a);
 	}
 }
