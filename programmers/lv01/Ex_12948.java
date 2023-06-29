50dda6db90ae3b56f5c8b556e430360baab6b09e
Kimsolchan <khj25830@naver.com>
2023-06-30 06:36:43 +0900

    programmers Ex_12948 modify

--- programmers/lv01/Ex_12948.java
+++ programmers/lv01/Ex_12948.java
@@ -1,24 +1,22 @@
 package codingtest.programmers.lv01;
 
 public class Ex_12948 {
-	public static String solution(String phone_number) {
+	public String solution(String phone_number) {
+		// 1. 문자열을 쪼개고, String형 배열에 값 저장
 		String[] strArr = phone_number.split("");
 		String answer = "";
 		
-		for(int i=strArr.length-5; i>=0; i--) {
+		// 2. 끝에서 5번째 요소 ~ 배열의 첫번째 요소까지 "*" 처리한다.
+		//    -> int i = strArr.length - 5;
+		for (int i = strArr.length - 5; i >= 0; i--) {
 			strArr[i] = "*";
 		}
 		
-		for(int i=0; i<strArr.length; i++) {
+		// 3. String형 변수에 배열의 모든 값을 더한다.
+		for (int i = 0; i < strArr.length; i++) {
 			answer += strArr[i];
 		}
-		
-		
+
 		return answer;
-	}
-	
-	public static void main(String[] args) {
-		String phone_number = "027778888"; // "027778888"
-		System.out.println(solution(phone_number));
 	}
 }
