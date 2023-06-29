2dc870b78b4c9e05a1bfaf392d9e0d0d9963971c
Kimsolchan <khj25830@naver.com>
2023-06-30 02:46:18 +0900

    programmers Ex_12919 commit

---
+++ programmers/lv01/Ex_12919.java
@@ -0,0 +1,17 @@
+package codingtest.programmers.lv01;
+
+public class Ex_12919 {
+	public String solution(String[] seoul) {
+		String answer = "";
+		
+		// 1. for문은 seoul에 길이만큼 반복한다.
+		for(int i=0; i<seoul.length; i++) {
+			// 2. seoul[i]인덱스 값이 "Kim"과 같다면
+			if(seoul[i].equals("Kim")) {
+				// anwer = 김서방은 i(인덱스번호)에 있다
+				answer = "김서방은 " + i + "에 있다";
+			}
+		}
+		return answer;
+	}
+}
