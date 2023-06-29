ba5e17a9af51598c06beb9e32a1247babb43866d
Kimsolchan <khj25830@naver.com>
2023-06-29 23:01:00 +0900

    programmers_Ex_12928 modify

--- programmers/lv01/Ex_12928.java
+++ programmers/lv01/Ex_12928.java
@@ -5,25 +5,16 @@ public class Ex_12928 {
     	// n을 나눌 약수
         int number = 1;
 		int answer = 0;
-        
-        while(true){
-        	// 1. number가 n보다 크면 
-            if(number > n){
-            	// while문 종료
-                break;
-            // 2. 조건1의 결과가 거짓이고,
-            //	  n % number == 0이면
-            }else if(n % number == 0){
-            	// answer = answer + number;
-                answer += number;
-                // number값 1증가
-                number++;
-            // 3. 조건2의 결과가 거짓이면    
-            }else{
-            	// number값 1증가
-                number ++;
-            }
-        }
+		
+		// for문은 i가 n과 같거나 작을때까지 반복한다.
+		// number은 for문이 한번 반복할 때마다 1씩 증가한다.
+		for(int i=1; i<=n; i++, number++) {
+			// 1. n을 number로 나눴을 때 나머지가 0이면
+			if(n % number == 0) {
+				// 2. answer = answer + number;
+				answer += number;
+			}
+		}
         return answer;
     }        
 }
